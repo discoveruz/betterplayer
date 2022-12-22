779005bc93cf7ec5299b1b6ee901d82ef41dde99
discoveruz <discover.diyorbek@gmail.com>
2022-12-22 19:11:10 +0500

    duplicate play button removed in material ui

--- lib/src/controls/better_player_material_controls.dart
+++ lib/src/controls/better_player_material_controls.dart
@@ -550,17 +550,11 @@ class _BetterPlayerMaterialControlsState
   Widget _buildPlayPause(VideoPlayerController controller) {
     return BetterPlayerMaterialClickableWidget(
       key: const Key("better_player_material_controls_play_pause_button"),
-      onTap: _onPlayPause,
+      onTap: () {},
       child: Container(
         height: double.infinity,
         margin: const EdgeInsets.symmetric(horizontal: 4),
         padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: Icon(
-          controller.value.isPlaying
-              ? _controlsConfiguration.pauseIcon
-              : _controlsConfiguration.playIcon,
-          color: _controlsConfiguration.iconsColor,
-        ),
       ),
     );
   }
